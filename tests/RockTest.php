2ddce8a878c7634869b531da01ade503ee06b25a
MBellJDD <marybelljd@gmail.com>
2022-08-23 09:12:19 +0200

    x

--- tests/RockTest.php
+++ tests/RockTest.php
@@ -2,4 +2,16 @@
 
 namespace Tests;
 
+use App\Models\Rock;
 use PHPUnit\Framework\TestCase;
+
+class RockTest extends TestCase
+{
+    public function test_is_a_rock()
+    {
+    $rock = new Rock();
+    $result = $rock->getType();
+    $this->assertEquals("Rock",$result);
+    }
+    
+}
